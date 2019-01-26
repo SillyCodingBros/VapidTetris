d9edc70e3a670c84663f498337898aa9a1f30682
Bobbywan Kenoby <leonoixdecoco@gmail.com>
2019-01-26 20:40:15 +0100

    add header.h

---
+++ head.h
@@ -0,0 +1,30 @@
+/* truc du head.h */
+
+#define WIDTH 20
+#define HEIGHT 25
+
+typedef struct {
+    char* data;
+    int size;
+    int len;
+}container;
+
+/* fonction de update.c */
+int* detectRaw(container* grid);
+char fullRaw(container* grid, int index);
+void deleteRaw(container* grid, int* indexList);
+int* detectCol(container* grid);
+char fullCol(container* grid, int index);
+void deleteCol(container* grid, int* indexList);
+void gravity(container* grid, int* index_list_line, int* index_list_col);
+void update(container* grid);
+
+/* fonction de container.c */
+void initContainer(int width, int heigth, container* item);
+void deleteContainer(container* item);
+//a completer avec les fonctions de generation pour chaque piece
+
+/* fonction de utilisateur.c */
+void affichage(container* grid);
+void rotate(container* item);
+//a completer avec les fonctions de placement et de collision
