0a09bcf4df20a6f0c61c2b5623f6c61be572966d
TheTitoPlay <vocatis.stark@gmail.com>
2019-02-01 11:58:31 +0100

    Le MAIN !!!!

--- main.c
+++ main.c
@@ -1,13 +1,28 @@
 #include "head.h"
 
-int main(int argc, char const *argv[])
-{
-	container playGrid, piece;
+int main(int argc, char const *argv[]){
+
+	container playGrid, playPiece;
+	int pieceX, pieceY;
+
 	initContainer(WIDTH,HEIGHT,&playGrid);
-	piece = randomPiece();
-	while(1){
+
+	playPiece = randomPiece();
+
+	int tmp=1;
+
+	while(tmp){
+
+		display(&playPiece);
 		display(&playGrid);
-		display(&piece);
+
+		printf("Enter x, y of top left corner ");
+		scanf("%d %d", &pieceX,&pieceY);
+		place(&playGrid,&playPiece,pieceX,pieceY);
+		update(&playGrid);
+		playPiece = randomPiece();
+		//printf("X and Y : %d %d\n", pieceX, pieceY);
+		//tmp = 0;
 	}
 	return 0;
 }
