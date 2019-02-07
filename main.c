1233e6e1b7bee99ecf0df4756328a3f9da926780
TheTitoPlay <vocatis.stark@gmail.com>
2019-02-07 23:44:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,28 +5,26 @@ int main(int argc, char const *argv[]){
 	container playGrid, playPiece;
 	int pieceX, pieceY;
 
-	initContainer(WIDTH,HEIGHT,&playGrid);
+	initContainer(WIDTH, HEIGHT, &playGrid);
 
 	playPiece = randomPiece();
 
-	int tmp=1;
+	int tmp = 1;
 
 	while(tmp){
 
 		display(&playPiece);
 		display(&playGrid);
 
-		printf("Enter x, y of top left corner ");
-		scanf("%d %d", &pieceX,&pieceY);
-		if (checkCollision(&playGrid,&playPiece,pieceX,pieceY)) {
-			printf("invalid place !\n");
+		printf("Enter 'x y' of top left corner: ");
+		scanf("%d %d", &pieceX, &pieceY);
+		if (checkCollision(&playGrid, &playPiece, pieceX, pieceY)) {
+			printf("Can't fit here!\n");
 		}else{
-			place(&playGrid,&playPiece,pieceX,pieceY);
+			place(&playGrid, &playPiece, pieceX, pieceY);
 			playPiece = randomPiece();
 		}
 		update(&playGrid);
-		//printf("X and Y : %d %d\n", pieceX, pieceY);
-		//tmp = 0;
 	}
 	return 0;
 }
